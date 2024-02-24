cd969b3b49697f560c3f85f23c0ecc231949075e
Christian <christianaguilar2223@gmail.com>
2024-02-23 21:28:16 -0500

    completed game with win funcionality and round loop

--- script.js
+++ script.js
@@ -9,17 +9,43 @@ const choices = [
     "Paper",
     "Scissors"
 ]
+//check winner function
+function checkWin() {
+    if(userScore === 3){
+        userScore = 0
+        computerScore = 0
+        tieScore = 0
+        console.log("User Wins Game Over")
 
+    }else if (computerScore === 3){
+        userScore = 0
+        computerScore = 0
+        tieScore = 0
+        console.log("Computer Wins Game Over")
+    }else if (tieScore === 3){
+        userScore = 0
+        computerScore = 0
+        tieScore = 0
+        console.log("Tie No Winner Try Again")
+    } 
+}
+
+//Scores 
+    let userScore = 0
+    let computerScore = 0
+    let tieScore = 0
+    let round = 0
+    while(userScore < 3 && computerScore < 3 && round < 12){
+        playGame()
+     }
 //function to play game 
 function playGame() {
     let userChoice = prompt("What will you choose Rock Paper or Scissors?").toLowerCase()
     let computerChoice = getComputerChoice();
-    let userScore = 0
-    let computerScore = 0
-    let tieScore = 0
+
     if(userChoice == "rock" && computerChoice == "paper"){
         computerScore++
-        console.log(`${computerChoice} beats ${userChoice} Computer Wins`)
+        console.log(`${computerChoice} beats ${userChoice} Computer Wins`) 
     }else if(userChoice == "rock" && computerChoice == "scissors"){
         userScore++
         console.log(`${userChoice} beats ${computerChoice} User Wins`)
@@ -40,11 +66,13 @@ function playGame() {
         console.log("it's a tie")
     }else if(userChoice == "scissors" && computerChoice == "rock"){
         computerScore++
+        
         console.log(`${computerChoice} beats ${userChoice} Computer Wins`)
     }else if (userChoice == "scissors" && computerChoice == "paper"){
         userScore++
         console.log(`${userChoice} beats ${computerChoice} user wins`)
     }else 
     console.log("not an option")
+    round++
 }
-playGame()
+checkWin()
